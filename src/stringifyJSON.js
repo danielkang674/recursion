b623219ce71a31ce19fbc8be645fc89a50602829
Daniel Kang <danielkang674@gmail.com>
2018-07-07 13:18:15 -0500

    v1 will check for primitive and array

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,34 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  let result = '';
+  if (typeof obj === 'function' || typeof obj === 'undefined') {
+    return undefined;
+  }
+  if (typeof obj === 'number' || typeof obj === 'boolean' || typeof obj === 'string' || obj === null) {
+    if (obj === null) {
+      result += 'null';
+    }
+    if (typeof obj === 'string') {
+      result += '"' + obj + '"';
+    }
+    if (typeof obj === 'number' || typeof obj === 'boolean') {
+      result += obj.toString();
+    }
+  }
+  if (Array.isArray(obj)) {
+    result += '[';
+    // let tempArr = [];
+    // for (let i = 0; i < obj.length; i++) {
+    //   tempArr.push(stringifyJSON(obj[i]));
+    // }
+    let tempArr = obj.map(function(value) {
+      return stringifyJSON(value);
+    });
+    let tempStr = tempArr.join(',');
+    result += tempStr;
+    result += ']';
+  }
+  return result;
 };
+console.log(stringifyJSON({a: 1, 2: 2, c: 'd'}));

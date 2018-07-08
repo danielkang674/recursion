87e59a002f1b627829e5e3491b39ed503185a5a5
Daniel Kang <danielkang674@gmail.com>
2018-07-07 23:19:20 -0500

    Create inner function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -13,7 +13,18 @@ var getElementsByClassName = function(className) {
     let temp = domNode.children;
     let tempArr = Array.from(temp);
     for (let value of tempArr) {
-      
+      if (value.hasChildNodes()) {
+        inner(className, value);
+      }
+      if (value.classList) {
+        let classArr = Array.from(value.classList);
+        for (let classItem of classArr) {
+          if (classItem === className) {
+            classNodes.push(value);
+            break;
+          }
+        }
+      }
     }
   }
 };

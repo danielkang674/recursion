1d669ef0686fe9871d8ce911cb6208346bbacc23
Daniel Kang <danielkang674@gmail.com>
2018-07-11 12:37:27 -0500

    Parse booleans & null and fix obj nesting

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -14,6 +14,9 @@ var parseJSON = function (json) {
       if (RegExp(/[0-9]/).test(str[0])) {
         return numFunc(str);
       }
+      if (str[0] === 't' || str[0] === 'f' || str[0] === 'n') {
+        return boolFunc(str);
+      }
       if (str[0] === '{') {
         return objFunc(str);
       } else {
@@ -38,9 +41,26 @@ var parseJSON = function (json) {
     return tempNum;
   };
 
-  // let boolFunc = function (str) {
-
-  // };
+  let boolFunc = function (str) {
+    if (str.indexOf('true') === 0) {
+      let endIndex = 4;
+      let tempBool = str.slice(0, endIndex);
+      copyJSON = copyJSON.slice(endIndex);
+      return true;
+    }
+    if (str.indexOf('false') === 0) {
+      let endIndex = 5;
+      let tempBool = str.slice(0, endIndex);
+      copyJSON = copyJSON.slice(endIndex);
+      return false;
+    }
+    if (str.indexOf('null') === 0) {
+      let endIndex = 4;
+      let tempBool = str.slice(0, endIndex);
+      copyJSON = copyJSON.slice(endIndex);
+      return null;
+    }
+  };
 
   let objFunc = function (str, existingObj) {
     let tempkey;
@@ -54,23 +74,39 @@ var parseJSON = function (json) {
       return existingObj;
     } else {
       let tempObj = {};
-      copyJSON = str.slice(1, str.length - 1);
+      copyJSON = str.slice(1, str.length);
       tempkey = strFunc(copyJSON);
       tempObj[tempkey] = next(copyJSON);
-      if (copyJSON[0] === ',') {
+      if (copyJSON[0] === ',' || (copyJSON[0] === '}' && copyJSON[1] === ',')) {
         tempObj = objFunc(copyJSON, tempObj);
       }
       return tempObj;
     }
   };
 
+  // let arrayFunc = function(str, existingArray){
+  //   let tempIndex;
+  //   if(existingArray){
+
+  //   } else {
+  //     let tempArray = [];
+  //     copyJSON = str.slice(1, str.length -1);
+  //     tempIndex = tempArray.length;
+  //     tempArray[tempIndex] = next(copyJSON);
+
+  //   }
+  // }
+
   result = next(copyJSON);
   return result;
 
 };
-console.log(parseJSON('{"a":2, "b": 34, "c":1}'));
-console.log(parseJSON('{"foo":"bar"}')); // correct output
-console.log(parseJSON('{"foo":{"bar":"bazz"}}')); // correct output
-console.log(parseJSON('{"a":"b", "c":"d"}')); // correct output
-console.log(parseJSON('{"a":"b", "c":{"d": "e"}}')); // correct output
-console.log(parseJSON('{"a":"b", "c":{"d": "e"}, "f": 1}')); // correct output
+// console.log(parseJSON('{"a":[1,2, 3], "b": [4,5,6],"c":null}'));
+// console.log(parseJSON('{"a":true, "b": false, "c":null}'));
+// console.log(parseJSON('{"a":2, "b": 34, "c":1}'));
+// console.log(parseJSON('{"foo":"bar"}')); // correct output
+// console.log(parseJSON('{"foo":{"bar":"bazz"}}')); // correct output
+// console.log(parseJSON('{"a":"b", "c":"d"}')); // correct output
+// console.log(parseJSON('{"a":"b", "c":{"d": "e"}}')); // correct output
+console.log(parseJSON('{"c":{"d": "e", "g": 5}, "f": 1}')); // output { c:{d: e} }
+console.log(parseJSON('{"f": 1, "g": 2, "c":{"d": "e"}}')); // correct output
